41983b6b8282ed257a18e562c7c8c3dfccfeb106
Shashank <langishashank@gmail.com>
2022-05-31 10:42:59 +0530

    [Shashank]:Creating Queue using LinkedList.

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -5,7 +5,28 @@ namespace StackAndQueues
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to Stack and Queue!!");
+            Console.WriteLine("Welcome to Stack and Queue!");
+
+            Node node1 = new Node(70);
+            Node node2 = new Node(30);
+            Node node3 = new Node(56);
+
+            //UC1 Create Stack
+            Stack stack = new Stack();
+            stack.Push(node1);
+            stack.Push(node2);
+            stack.Push(node3);
+            stack.DisplayStack();
+
+            //UC2 POP Stack
+            stack.Pop(node1);
+            stack.DisplayStack();
+            stack.Pop(node2);
+            stack.DisplayStack();
+            stack.Pop(node3);
+            stack.DisplayStack();
+            
+            //UC3 Create Queue
             Queue queue = new Queue();
             queue.Enqueue(56);
             queue.Enqueue(30);
